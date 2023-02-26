10db7237d103bacea6544a7b7bcd63e2148e28ea
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-02-26 16:02:46 +0200

    fetchCountries function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
+import fetchCountries from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix'
 
 const DEBOUNCE_DELAY = 300;
+const countryInput = document.querySelector("#search-box");
+
+countryInput.addEventListener("input", debounce(onCountryInput, DEBOUNCE_DELAY));
+
+function onCountryInput(){}
